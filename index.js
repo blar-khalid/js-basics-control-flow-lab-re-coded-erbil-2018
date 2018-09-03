5ae000262c0070556023004e155f57df731dd173
blar khalid <blarharki@gmail.com>
2018-09-03 10:04:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ function ternaryCheckCity(city){
 
   
 }
+function switchOnCharmFromTip(tip){
+  if(tip==='generous'){
+  return  "Thank you so much." 
+  }else if(tip === 'not as generous'){
+    return "Thank you."
+  }else{
+    return "Bye."
+  }
+}
+
+
