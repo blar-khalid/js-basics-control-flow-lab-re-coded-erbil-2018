b3ca771a7e2623ea1791ace3bb71da97ed5190a7
blar khalid <blarharki@gmail.com>
2018-09-03 09:59:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ function scuberGreetingForFeet(param)
   }
 }
 function ternaryCheckCity(param1){
-  if(param1==NYC){}
+  if(param1==NYC){
+    return  "Ok, sounds good." 
+  }
   
-  else
+  else{
+    
+  }
   
 }
